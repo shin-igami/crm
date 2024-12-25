e8ec9fe1ad8cf45c469aef308578571ca6119d66
Sagil Faraz <faraz15799@gmail.com>
2024-12-25 16:52:09 +0530

    fronetend

--- frontend/src/Components/LeadDetails/LeadDetails.js
+++ frontend/src/Components/LeadDetails/LeadDetails.js
@@ -3,10 +3,51 @@ import React from 'react'
 function LeadDetails() {
     return (
         <div>
-            <div className="LeadInformation " style={{ backgroundColor: 'white' }}>
+                 <div className="LeadInformation  py-2  " style={{ backgroundColor: 'white' }}>
              
-                <div className="rows d-flex   my-2 w-75 mx-auto">
-                <h2>Details</h2>
+             <div className="rows d-flex   my-2 w-75 mx-auto border-bottom border-secondary border-1">
+             <h3>Lead Details</h3>
+             </div>
+             <div className="rows d-flex   my-2 w-75 mx-auto">
+                 <div class=" d-flex  " style={{ width: "40%", height: "fit-content" }}>
+                     <p className=' me-4 my-0 fw-semibold  ' style={{ height: "fit-content" }} >Lead Owner</p>
+                     <p className=' my-0    ' style={{ height: "fit-content" }}> Maanof Group</p>
+                 </div>
+             
+
+             </div>
+             <div className="rows d-flex   my-2 w-75 mx-auto">
+                 <div class=" d-flex  " style={{ width: "40%", height: "fit-content" }}>
+                     <p className=' me-4 my-0 fw-semibold  ' style={{ height: "fit-content" }} > Email</p>
+                     <p className=' my-0    ' style={{ height: "fit-content" }}> sagilfaraz@15gmail.com</p>
+                 </div>
+             
+
+             </div>
+             <div className="rows d-flex   my-2 w-75 mx-auto">
+                 <div class=" d-flex  " style={{ width: "40%", height: "fit-content" }}>
+                     <p className=' me-4 my-0 fw-semibold  ' style={{ height: "fit-content" }} > Phone</p>
+                     <p className=' my-0    ' style={{ height: "fit-content" }}> 8709057771</p>
+                 </div>
+              
+
+             </div>
+             <div className="rows d-flex   my-2 w-75 mx-auto">
+                 <div class=" d-flex  " style={{ width: "40%", height: "fit-content" }}>
+                     <p className=' me-4 my-0 fw-semibold  ' style={{ height: "fit-content" }} > Lead Status</p>
+                     <p className=' my-0    ' style={{ height: "fit-content" }}> InProgress</p>
+                 </div>
+          
+
+             </div>
+        
+    
+
+         </div>
+            <div className="LeadInformation my-4" style={{ backgroundColor: 'white' }}>
+             
+                <div className="rows d-flex   my-2 w-75 mx-auto border-bottom border-secondary border-1">
+                <h3>Hide Details</h3>
                 </div>
                 <div className="rows d-flex   my-2 w-75 mx-auto">
                     <div class=" d-flex  " style={{ width: "40%", height: "fit-content" }}>
@@ -97,71 +138,27 @@ function LeadDetails() {
 
                 </div>
        
-
-            </div>
-            <div className="LeadInformation my-4" style={{ backgroundColor: 'white' }}>
              
-                <div className="rows d-flex   my-2 w-75 mx-auto">
-                <h5>Adress Information</h5>
-                </div>
-                <div className="rows d-flex   my-2 w-75 mx-auto">
-                    <div class=" d-flex  " style={{ width: "40%", height: "fit-content" }}>
-                        <p className=' me-4 my-0 fw-semibold  ' style={{ height: "fit-content" }} >Street</p>
-                        <p className=' my-0    ' style={{ height: "fit-content" }}> Main Road</p>
-                    </div>
-                    <div class=" d-flex   " style={{ width: "40%", height: "fit-content" }}>
-                        <p className='   me-4 my-0 fw-semibold  ' style={{ height: "fit-content" }} > City</p>
-                        <p className=' my-0    ' style={{ height: "fit-content" }}> New Delhi</p>
-                    </div>
-
-                </div>
-                <div className="rows d-flex   my-2 w-75 mx-auto">
-                    <div class=" d-flex  " style={{ width: "40%", height: "fit-content" }}>
-                        <p className=' me-4 my-0 fw-semibold  ' style={{ height: "fit-content" }} > State</p>
-                        <p className=' my-0    ' style={{ height: "fit-content" }}> Delhi</p>
-                    </div>
-                    <div class=" d-flex   " style={{ width: "40%", height: "fit-content" }}>
-                        <p className='   me-4 my-0 fw-semibold  ' style={{ height: "fit-content" }} > Zip Code</p>
-                        <p className=' my-0    ' style={{ height: "fit-content" }}> 847874</p>
-                    </div>
-
-                </div>
-                <div className="rows d-flex   my-2 w-75 mx-auto">
-                    <div class=" d-flex  " style={{ width: "40%", height: "fit-content" }}>
-                        <p className=' me-4 my-0 fw-semibold  ' style={{ height: "fit-content" }} > Country</p>
-                        <p className=' my-0    ' style={{ height: "fit-content" }}> India</p>
-                    </div>
-                    <div class=" d-flex   " style={{ width: "40%", height: "fit-content" }}>
-                        <p className='   me-4 my-0 fw-semibold  ' style={{ height: "fit-content" }} > Maanof Pvt Ltd</p>
-                        <p className=' my-0    ' style={{ height: "fit-content" }}> Maanof Pvt Ltd</p>
-                    </div>
-
-                </div>
-                <div className="rows d-flex   my-2 w-75 mx-auto">
-                    <div class=" d-flex  " style={{ width: "40%", height: "fit-content" }}>
-                        <p className=' me-4 my-0 fw-semibold  ' style={{ height: "fit-content" }} > Maanof Pvt Ltd</p>
-                        <p className=' my-0    ' style={{ height: "fit-content" }}> Maanof Pvt Ltd</p>
-                    </div>
-                    <div class=" d-flex   " style={{ width: "40%", height: "fit-content" }}>
-                        <p className='   me-4 my-0 fw-semibold  ' style={{ height: "fit-content" }} > Maanof Pvt Ltd</p>
-                        <p className=' my-0    ' style={{ height: "fit-content" }}> Maanof Pvt Ltd</p>
-                    </div>
-
-                </div>
-                <div className="rows d-flex   my-2 w-75 mx-auto">
-                    <div class=" d-flex  " style={{ width: "40%", height: "fit-content" }}>
-                        <p className=' me-4 my-0 fw-semibold  ' style={{ height: "fit-content" }} > Maanof Pvt Ltd</p>
-                        <p className=' my-0    ' style={{ height: "fit-content" }}> Maanof Pvt Ltd</p>
-                    </div>
-                    <div class=" d-flex   " style={{ width: "40%", height: "fit-content" }}>
-                        <p className='   me-4 my-0 fw-semibold  ' style={{ height: "fit-content" }} > Maanof Pvt Ltd</p>
-                        <p className=' my-0    ' style={{ height: "fit-content" }}> Maanof Pvt Ltd</p>
-                    </div>
-
-                </div>
-       
-
             </div>
+            <div className="LeadInformation my-4 py-2" style={{ backgroundColor: 'white' }}>
+             
+             <div className="rows d-flex   my-2 w-75 mx-auto border-bottom border-secondary border-1">
+             <h3>Product</h3>
+             </div>
+             <div className="rows d-flex my-2 w-75 mx-auto border-bottom border-secondary border-1">
+            <div className="items">Product Name </div>
+            <div className="items">Product Code </div>
+            <div className="items">Product Active  </div>
+            <div className="items">Manufacturer</div>
+            <div className="items">Support Start Date</div>
+            <div className="items">Support End Date</div>
+          
+           </div>
+       
+          
+    
+
+         </div>
         </div>
     )
 }
